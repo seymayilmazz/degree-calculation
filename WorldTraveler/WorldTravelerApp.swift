39fbe01db0f496df00a31096bff26226e67e70ff
seyma.yilmaz <seyma.yilmaz@trendyol.com>
2021-01-01 22:21:35 +0300

    Initial Commit

---
+++ WorldTraveler/WorldTravelerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTravelerApp.swift
+//  WorldTraveler
+//
+//  Created by Şeyma Yılmaz on 1.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct WorldTravelerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
